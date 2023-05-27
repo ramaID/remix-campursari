ea9338fa1cd2de397cd6e0b3a3fcffb96bd3fa2c
rama <rama@javan.co.id>
2023-05-27 14:22:33 +0700

    setup shadcn/ui

--- app/root.tsx
+++ app/root.tsx
@@ -15,14 +15,14 @@ export const links: LinksFunction = () => [
 
 export default function App() {
   return (
-    <html lang="en">
+    <html lang="en" className="h-full bg-gray-100">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
         <Meta />
         <Links />
       </head>
-      <body>
+      <body className="h-full">
         <Outlet />
         <ScrollRestoration />
         <Scripts />
